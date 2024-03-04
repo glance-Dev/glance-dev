a0a478cb52785a0766d684bf0fcb8cb11f1c34ee
AbdullahOmar97 <AbdullahOmar@outlook.com>
2024-03-04 07:19:28 +0000

    Header & Footer v2

--- glance-dev/src/App.js
+++ glance-dev/src/App.js
@@ -2,26 +2,25 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/home/Home';
-import Contact from './components/contact/Contact';
-import About from './components/about/About';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 
 function App() {
-  return (
+  return ( 
+    <>
+    edfmek
    <Router>
    <Header />
-   <Home/>
    <Routes>
      <Route path="/" element={ <Home/> } />
-     <Route path="about" element={ <About/> } />
-     <Route path="contact" element={ <Contact/> } />
+
    </Routes>
 
    <Footer />
 
  </Router>
+ </>
   );
 }
 
